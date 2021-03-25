ae860b770056dc1e644fa0a54ec1cb6379239b8f
Katie Corrion <corrion.katie@gmail.com>
2021-03-25 12:50:58 -0400

    final

--- StarterCode/app.js
+++ StarterCode/app.js
@@ -1,18 +1,64 @@
-function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
-      var metadata = data.metadata;
-      // Filter the data for the object with the desired sample number
-      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = resultArray[0];
-      // Use d3 to select the panel with id of `#sample-metadata`
-      var PANEL = d3.select("#sample-metadata");
-  
-      // Use `.html("") to clear any existing metadata
-      PANEL.html("");
-  
-      // Use `Object.entries` to add each key and value pair to the panel
-      // Hint: Inside the loop, you will need to use d3 to append new
-      // tags for each key-value in the metadata.
-      Object.entries(result).forEach(([key, value]) => {
-        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+function getPlots(id) {
+  d3.json("samples.json").then (sampledata =>{
+      var ids = sampledata.samples[0].otu_ids;
+      var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+      var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+      var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+      var OTU_id = OTU_top.map(d => "OTU " + d);
+      var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+      var trace = {
+          x: sampleValues,
+          y: OTU_id,
+          text: labels,
+          marker: {
+          color: 'blue'},
+          type:"bar",
+          orientation: "h",
+      };
+      var data = [trace];
+      var layout = {
+          title: "Top 10 OTU",
+          yaxis:{
+              tickmode:"linear",
+          },
+          margin: {
+              l: 100,
+              r: 100,
+              t: 100,
+              b: 30
+          }
+      };
+  Plotly.newPlot("bar", data, layout);
+      var trace1 = {
+          x: sampledata.samples[0].otu_ids,
+          y: sampledata.samples[0].sample_values,
+          mode: "markers",
+          marker: {
+              size: sampledata.samples[0].sample_values,
+              color: sampledata.samples[0].otu_ids
+          },
+          text:  sampledata.samples[0].otu_labels
+      };
+      var layout_2 = {
+          xaxis:{title: "OTU ID"},
+          height: 600,
+          width: 1000
+      };
+      var data1 = [trace1];
+  Plotly.newPlot("bubble", data1, layout_2);
+  });
+}
+function optionChanged(id) {
+  getPlots(id);
+}
+function init() {
+  var dropdown = d3.select("#selDataset");
+  d3.json("samples.json").then((data)=> {
+      console.log(data)
+      data.names.forEach(function(name) {
+          dropdown.append("option").text(name).property("value");
       });
+      getPlots(data.names[0]);
+  });
+};
+init();
